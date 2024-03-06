df40dd2605194339e09c2b451c8d7ed28a6e8659
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-03-06 16:20:04 +1300

    Added configuration values for index

--- k3s-infra/index.ts
+++ k3s-infra/index.ts
@@ -1,3 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
-// Hello
-var hello = pulumi.config;
+
+// Obtain pulumi configuration
+const config = new pulumi.Config();
+
+// Obtain secret ssh key from config
+const serverKey = config.requireSecret("serverKey");
+
+// Obtain server IP from config
+const serverIp = config.requireSecret("serverIp");
