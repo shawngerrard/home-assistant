ea46bf97744b1b1c0afc2b5571c726815c1331d1
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-03-12 13:25:34 +1300

    Added pulumi/command to package.json

--- k3s-infra/index.ts
+++ k3s-infra/index.ts
@@ -1,21 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export function pulumiConfig(): string {
+// Obtain pulumi configuration
+const config = new pulumi.Config("k3s-infra");
 
-    // Obtain pulumi configuration
-    const config = new pulumi.Config("k3s-infra");
+// Export secret ssh key from config to output
+export const serverKey = config.requireSecret("serverKey");
 
-    // Obtain secret ssh key from config
-    const serverKey = config.requireSecret("serverKey");
+// Export server IP from config to output
+export const serverIp = config.require("serverIp");
 
-    // Obtain server IP from config
-    const serverIp = config.require("serverIp");
-
-    // Output
-    console.log(`serverKey is ${serverKey} and serverIp is ${serverIp}`);
-
-    //return `serverKey is ${serverKey} and serverIp is ${serverIp}
-    return `serverIp is ${serverIp}`
-}
-
-export const serverIp = pulumiConfig();
+// Log config
+//console.log(`serverKey is ${serverKey} and serverIp is ${serverIp}`);
+console.log(config);
