cd3482f1bc0d8be7661c269141981810a710bc11
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-03-16 19:14:43 +1300

    Removed interfaces from git ignore

---
+++ k3s-infra/bin/interfaces.ts
@@ -0,0 +1,7 @@
+/* This file defines the interfacing types for object maps used throughout the project */
+export interface iConnectionObj {
+  host: string;
+  port: number;
+  user: string;
+  privateKey: string;
+}
