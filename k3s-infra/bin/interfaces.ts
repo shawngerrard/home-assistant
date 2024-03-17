98b2cdfb9f9eeadabf9c0ffa8b08b0ef2b1ce1f2
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-03-18 11:01:52 +1300

    Fixed iConnectionObj interface

--- k3s-infra/bin/interfaces.ts
+++ k3s-infra/bin/interfaces.ts
@@ -1,7 +1,10 @@
+import * as pulumi from "@pulumi/pulumi";
+
+
 /* This file defines the interfacing types for object maps used throughout the project */
 export interface iConnectionObj {
   host: string;
   port: number;
   user: string;
-  privateKey: string;
+  privateKey: pulumi.Output<string>;
 }
