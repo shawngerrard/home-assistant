355aa13b1d79b4b5b683f4762d525fcc2ad6c2fd
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-03-28 00:20:08 +1300

    Refactored connection referencecss symbol

--- app-homeassistant/index.ts
+++ app-homeassistant/index.ts
@@ -7,14 +7,14 @@ const config = new pulumi.Config("app-homeassistant");
 // Obtain reference to the infra-k3s stack
 const stackRef = new pulumi.StackReference(`${config.require("org")}/${config.require("serverStack")}/dev`);
 // Obtain and output server stack references
-export const conn = stackRef.getOutput('connectionConfig');
+export const serverConnectionConfig = stackRef.getOutput('connectionConfig');
 // Obtain server connection values from config and stack references
 const connectionObj: iConnectionObj = {
-  host: conn.apply(conn => {
+  host: serverConnectionConfig.apply(conn => {
     return conn.serverIp
   }),
   port: 22,
-  user: conn.apply(conn => {
+  user: serverConnectionConfig.apply(conn => {
     return conn.serverUser
   }),
   privateKey: config.requireSecret("serverKey")
