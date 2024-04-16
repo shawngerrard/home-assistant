09b4d9a48bb677e59dc97969edcb4437d06d0223
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-04-16 18:10:11 +1200

    Updated gcp with new service account definition

--- infra-gcp/index.ts
+++ infra-gcp/index.ts
@@ -1,10 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: "US"
-});
-
-// Export the DNS name of the bucket
-export const bucketName = bucket.url;
+async function main() {
+    // Obtain the current project
+    const projectId = pulumi.output(gcp.organizations.getProject({})).apply(project => project.projectId);
+    // Create a new service account in gcp
+    const serviceAccount = new gcp.serviceaccount.Account("home-assistant-service-account", {
+        accountId: "ha-admin",
+        displayName: "Home Assistant Admin",
+    });
+    // Grant roles to the service account
+    const tokenCreatorRoleBinding = new gcp.projects.IAMBinding("service-account-token-creator-policy", {
+        project: projectId,
+        role: "roles/iam.serviceAccountTokenCreator",
+        members: [serviceAccount.email],
+    });
+}
