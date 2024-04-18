e8d449cc78243b0c44faf86707f861bf7095ec07
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-04-18 15:13:53 +1200

    Added gcp service account key creation

--- infra-gcp/index.ts
+++ infra-gcp/index.ts
@@ -4,6 +4,7 @@ import * as gcp from "@pulumi/gcp";
 async function main() {
     // Obtain the current project
     const projectId = pulumi.output(gcp.organizations.getProject({})).apply(project => { return project.projectId });
+
     // Create a new service account in gcp
     const serviceAccount = new gcp.serviceaccount.Account("home-assistant-service-account", {
         accountId: "ha-admin-service-account",
@@ -17,10 +18,19 @@ async function main() {
     },{
         dependsOn: serviceAccount
     });
+    // TODO: Implement key rotation
+    // TODO: Implement hashicorp vault into infra-k3s to manage keys/rotation
+    // Create a key for the service account
+    const serviceAccountKey = new gcp.serviceaccount.Key("ha-admin-service-account-key",{
+        serviceAccountId: serviceAccount.name,
+        publicKeyType: "TYPE_X509_PEM_FILE"
+    });
     // Return output
     return {
         serviceAccountId: serviceAccount.accountId,
-        serviceAccountRoles: tokenCreatorRoleBinding.role
+        serviceAccountRoles: tokenCreatorRoleBinding.role,
+        serviceAccountKeyName: serviceAccountKey.name
     }
 }
+// Invoke main and output logs
 export const serviceAccountDetails = main();
