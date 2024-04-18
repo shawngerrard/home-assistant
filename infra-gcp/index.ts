3c23eecad4557a0c66a8d7670ca36e60f4133aa3
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-04-18 14:25:22 +1200

    Fixed error with policy member reference

--- infra-gcp/index.ts
+++ infra-gcp/index.ts
@@ -3,16 +3,24 @@ import * as gcp from "@pulumi/gcp";
 
 async function main() {
     // Obtain the current project
-    const projectId = pulumi.output(gcp.organizations.getProject({})).apply(project => project.projectId);
+    const projectId = pulumi.output(gcp.organizations.getProject({})).apply(project => { return project.projectId });
     // Create a new service account in gcp
     const serviceAccount = new gcp.serviceaccount.Account("home-assistant-service-account", {
-        accountId: "ha-admin",
+        accountId: "ha-admin-service-account",
         displayName: "Home Assistant Admin",
     });
     // Grant roles to the service account
     const tokenCreatorRoleBinding = new gcp.projects.IAMBinding("service-account-token-creator-policy", {
-        project: projectId,
+        project: projectId.apply(projectId => projectId || ""),
         role: "roles/iam.serviceAccountTokenCreator",
-        members: [serviceAccount.email],
+        members: [serviceAccount.member],
+    },{
+        dependsOn: serviceAccount
     });
+    // Return output
+    return {
+        serviceAccountId: serviceAccount.accountId,
+        serviceAccountRoles: tokenCreatorRoleBinding.role
+    }
 }
+export const serviceAccountDetails = main();
