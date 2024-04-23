9ec78a1278afb61b1210426a056cc682077f32c7
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-04-24 10:04:07 +1200

    Scaffolded app-nginx and refactored app-ha

--- app-nginx/index.ts
+++ app-nginx/index.ts
@@ -2,6 +2,9 @@
  * This stack depends on the deployment of the "infra-k3s" stack.
  *
  * Please ensure the "infra-k3s" stack is UP before deploying this stack.
+ *
+ * This will likely also need to be deployed prior to the "app-homeassistant" stack.
+ *
 */
 
 import * as pulumi from "@pulumi/pulumi";
@@ -9,22 +12,21 @@ import * as k8s from "@pulumi/kubernetes";
 import { getInfraStackConfig } from "../bin/functions/infraConfig";
 
 async function main() {
-  // Obtain the infra-k3s stack configuration
-  const infraConfigObj = await getInfraStackConfig();
   // Set the path for the local chart
-  const chartPath = "./../../helm-charts/charts/home-assistant";
+  const chartPath = "./../../helm-charts/charts/nginx-ingress";
   // Set the namespace name to create/use for the chart
-  // TODO: Abstract creation of chartNamespace to a higher-level so that this can be reused - perhaps infra-k3s? E.G `${config.namespace}-${pulumi.getStack()}`
   const chartNamespace = `home-assistant-${pulumi.getStack()}`;
-  // (Deprecated - added to app-nginx) Create a k3s namespace for the app
-  /*const createNamespace = new k8s.core.v1.Namespace(`Create ${chartNamespace} namespace`, {
+  // Create a k3s namespace that nginx will manage
+  const createNamespace = new k8s.core.v1.Namespace(`Create ${chartNamespace} namespace`, {
     metadata: {
       name: chartNamespace
     }
-  });*/
+  });
+  const infraConfigObj = await getInfraStackConfig();
   // Specify custom template settings for service in Helm values file
   // TODO: Update home-assistant helm chart values file to accept the following properties (rather than update here?)
   // TODO: Update NodePort to use either a LoadBalancer service or a ClusterIP service using an ingress resource
+  /* These need to be updated to suit nginx
   const customServiceValues = {
     service: {
       type: "NodePort",
@@ -40,14 +42,17 @@ async function main() {
   };
   // Deploy the home-assistant local chart
   // TODO: Update helm charts repo as environment-specific multi-repo
-  const appChart = new k8s.helm.v3.Chart("home-assistant",{
+  const appChart = new k8s.helm.v3.Chart("nginx",{
     path: chartPath,
     // TODO: Update infra and deployment repo into environment-specific multi-repo
     namespace: chartNamespace,
     values: customServiceValues
+  },{
+    dependsOn: createNamespace
   });
   // Return the connection object for output (testing)
-  return customServiceValues;
+  */
+  return "";
 }
 // Export the custom values supplied to the helm chart
-export const customServiceValues = main();
+//export const customServiceValues = main();
