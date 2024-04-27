7fda006f97aa1d2962821381ff6b619a07318e68
Shawn Gerrard <shawn.gerrard@gmail.com>
2024-04-27 17:23:33 +1200

    Added app-certmanager deployment code

--- app-certmanager/index.ts
+++ app-certmanager/index.ts
@@ -6,6 +6,7 @@
  * This will likely also need to be deployed prior to the "app-homeassistant" stack.
  *
 */
+
 import { Chart } from "@pulumi/kubernetes/helm/v3";
 import { getInfraStackConfig } from "../bin/functions/infraConfig";
 
@@ -13,16 +14,16 @@ async function main() {
   // Obtain the infra-k3s config via stack references
   const infraConfigObj = await getInfraStackConfig();
   // Set the path for the local chart
-  const chartPath = "./../../helm-charts/charts/nginx-ingress";
+  const chartPath = "./../../helm-charts/charts/cert-manager";
   // Deploy the home-assistant local chart
   // TODO: Update helm charts repo as environment-specific multi-repo
-  const appChart = new Chart("nginx",{
+  const appChart = new Chart("cert-manager",{
     path: chartPath,
     // TODO: Update infra and deployment repo into environment-specific multi-repo
     namespace: infraConfigObj.homeAssistantNamespace
   });
   // Return the connection object for output (testing)
-  return "";
+  return ""
 }
 // Export the custom values supplied to the helm chart
 export const customServiceValues = main();
